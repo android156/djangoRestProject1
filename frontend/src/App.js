81a4b55654541cc2be3493993634fe42b0d6eb47
android156 <n_degtyarev@mail.ru>
2022-05-13 19:22:50 +0300

    axios.get('http://127.0.0.1:8000/api/users') don't work.
    But this work:
    axios.get('http://127.0.0.1:8000/api/users/?format=json')

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
     // }
 
     componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users')
+        axios.get('http://127.0.0.1:8000/api/users/?format=json')
             .then(response => {
                 const users = response.data
                 this.setState(
