846bb92fdeea029839dd43d9be4e16cadc7c3b32
android156 <n_degtyarev@mail.ru>
2022-05-13 19:17:15 +0300

    added response from backend to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import UserList from './components/User.js'
+import axios from 'axios'
 
 
 class App extends React.Component {
@@ -12,32 +13,46 @@ class App extends React.Component {
         }
     }
 
+    // Заглушка, которая юзеров грузит из списка
+
+    // componentDidMount() {
+    //     const users = [
+    //         {
+    //             'first_name': 'Иван',
+    //             'last_name': 'Коротышкин',
+    //             'user_name': 'korotkiy',
+    //             'email': 'korotkiy@mail.ru',
+    //         },
+    //         {
+    //             'first_name': 'Админ',
+    //             'last_name': 'Админов',
+    //             'user_name': 'admin',
+    //             'email': 'admin@mail.ru',
+    //         },
+    //     ]
+    //     this.setState(
+    //         {
+    //             'users': users
+    //             }
+    //     )
+    // }
+
     componentDidMount() {
-        const users = [
-            {
-                'first_name': 'Иван',
-                'last_name': 'Коротышкин',
-                'user_name': 'korotkiy',
-                'email': 'korotkiy@mail.ru',
-            },
-            {
-                'first_name': 'Админ',
-                'last_name': 'Админов',
-                'user_name': 'admin',
-                'email': 'admin@mail.ru',
-            },
-        ]
-        this.setState(
-            {
-                'users': users
-                }
-        )
+        axios.get('http://127.0.0.1:8000/api/users')
+            .then(response => {
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+            }).catch(error => console.log(error))
     }
 
     render() {
         return (
             <div>
-                <UserList users={this.state.users} />
+                <UserList users={this.state.users}/>
             </div>
         )
     }
