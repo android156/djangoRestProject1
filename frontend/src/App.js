9af5f2dcaa429148e0e1eece48fa1b45d975dc36
android156 <n_degtyarev@mail.ru>
2022-05-13 19:03:32 +0300

    added stub in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import UserList from './components/User.js'
 
 
 class App extends React.Component {
@@ -11,10 +12,32 @@ class App extends React.Component {
         }
     }
 
+    componentDidMount() {
+        const users = [
+            {
+                'first_name': 'Иван',
+                'last_name': 'Коротышкин',
+                'user_name': 'korotkiy',
+                'email': 'korotkiy@mail.ru',
+            },
+            {
+                'first_name': 'Админ',
+                'last_name': 'Админов',
+                'user_name': 'admin',
+                'email': 'admin@mail.ru',
+            },
+        ]
+        this.setState(
+            {
+                'users': users
+                }
+        )
+    }
+
     render() {
         return (
             <div>
-                Main App
+                <UserList users={this.state.users} />
             </div>
         )
     }
