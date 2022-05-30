82142406700224f02ec32b8b79e364a0e96cba2e
android156 <n_degtyarev@mail.ru>
2022-05-30 20:59:25 +0300

    APITestCase added

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -1,10 +1,11 @@
-import json
 from django.test import TestCase
 from mixer import factory
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
 from mixer.backend.django import mixer
 from django.contrib.auth.models import User
+
+from .models import Project
 from .views import ToDoModelViewSet, ProjectModelViewSet
 from userapp.views import AppUserModelViewSet
 from userapp.models import AppUser
@@ -20,72 +21,20 @@ class TestProjectViewSet(TestCase):
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_guest(self):
-        factory = APIRequestFactory()
-        request = factory.post('/api/users/',
-                               {
-                                   'first_name': 'Юра',
-                                   'last_name': 'Лужков',
-                                   'user_name': 'luzhok',
-                                   'email': 'luzhok@moscow.ru',
-                                   'user_category': AppUser.MANAGER_STATUS,
-                               }, format='json')
-        view = AppUserModelViewSet.as_view({'post': 'create'})
-        response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_create_admin(self):
-        factory = APIRequestFactory()
-
-        request = factory.post('/api/users/',
-                               {
-                                   'first_name': 'Юра',
-                                   'last_name': 'Лужков',
-                                   'user_name': 'luzhok',
-                                   'email': 'luzhok@moscow.ru',
-                                   'user_category': AppUser.MANAGER_STATUS,
-                               }, format='json')
-        admin = User.objects.create_superuser('admin156', 'admin@admin.com',
-                                              'admin123456')
-        force_authenticate(request, admin)
-        view = AppUserModelViewSet.as_view({'post': 'create'})
-        response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_get_detail(self):
-        user = AppUser.objects.create(first_name='Юра', last_name='Лужков', user_name='luzhok',
-                                        email='luzhok@moscow.ru', user_category=AppUser.MANAGER_STATUS)
-        client = APIClient()
-        response = client.get(f'/api/users/{user.uid}/')
+class TestProjectViewSet2(APITestCase):
+    def test_get_list(self):
+        response = self.client.get('/api/projects/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data)
-
-    def test_edit_guest(self):
-        user = AppUser.objects.create(first_name='Юра', last_name='Лужков', user_name='luzhok',
-                                      email='luzhok@moscow.ru', user_category=AppUser.MANAGER_STATUS)
-
-        client = APIClient()
-        response = client.put(f'/api/users/{user.uid}/', {
-                                   'first_name': 'Ваня',
-                                   'last_name': 'Внуков',
-                               })
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        print(response.data)
 
     def test_edit_admin(self):
         user = AppUser.objects.create(first_name='Юра', last_name='Лужков', user_name='luzhok',
                                       email='luzhok@moscow.ru', user_category=AppUser.MANAGER_STATUS)
-        print(user)
-        client = APIClient()
+        project = Project.objects.create(name='Точечная застройка')
+        print(project)
         admin = User.objects.create_superuser('admin156', 'admin@admin.com', 'admin123456')
-        client.login(username='admin156', password='admin123456')
-        response = client.patch(f'/api/users/{user.uid}/', {
-            'first_name': 'Ваня',
-            'last_name': 'Внуков',
-        })
+        self.client.login(username='admin156', password='admin123456')
+        response = self.client.put(f'/api/projects/{project.uid}/', {'name': 'Ренновация'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        user = AppUser.objects.get(uid=user.uid)
-        self.assertEqual(user.first_name, 'Ваня')
-        self.assertEqual(user.last_name, 'Внуков')
-        print(response.data)
-        client.logout()
+        project = Project.objects.get(uid=project.uid)
+        self.assertEqual(project.name, 'Ренновация')
