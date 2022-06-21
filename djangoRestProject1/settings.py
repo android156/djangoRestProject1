2284d288c3d00ca58d74bcc0fd06bff114fc5e75
android156 from Linux <n_degtyarev@mail.ru>
2022-06-21 15:53:48 +0300

    Docker works

--- djangoRestProject1/settings.py
+++ djangoRestProject1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,9 @@ SECRET_KEY = 'django-insecure-7a453iyi&^iqh&!54=09ttioc49d!iwgwbynfh@il4p=qil_(8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -94,8 +97,14 @@ WSGI_APPLICATION = 'djangoRestProject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        
+        'NAME': 'db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'admin',
+        'PASSWORD': 'adMin9311395',
+        'HOST': 'localhost'
     }
 }
 
@@ -132,6 +141,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static/',
+)
+
+
+# STATIC_ROOT = path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
